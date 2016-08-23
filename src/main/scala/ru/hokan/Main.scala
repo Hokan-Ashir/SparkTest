12f67c3008c33201366343da7777334bd90292dd
Anton_Achkasov <achkasov.anton.92@gmail.com>
2016-08-23 09:41:24 +0300

    HW_1, added real tests and #2 screenshot, cause all test passed

--- src/main/scala/ru/hokan/Main.scala
+++ src/main/scala/ru/hokan/Main.scala
@@ -13,28 +13,38 @@ object Main {
   val FILE_NAME = "000000"
   val APPLICATION_NAME: String = "Simple Application"
   val REGEX_PATTERN = "(ip\\d+)\\s-\\s-\\s\\[.*\\]\\s\".*\"\\s\\d+\\s(\\d+)?.*".r
-  val PARTS_FILE_NAME_PREFIX: String = "hdfs:///user/maria_dev/temp"
-  val RESULT_FILE_NAME: String = "hdfs:///user/maria_dev/result.csv"
+  val PARTS_FILE_NAME_PREFIX: String = "temp"
+  val RESULT_FILE_NAME: String = "result.csv"
 
   def main(args: Array[String]) {
+    val (sc: SparkContext, cache: RDD[(String, Long)]) = extractIPAndBytesAmount
+    val map: Array[String] = countAverageByteNumberValues(cache)
+    map.foreach(println)
+    saveResultToFile(sc, map)
+  }
+
+  def extractIPAndBytesAmount: (SparkContext, RDD[(String, Long)]) = {
     val conf = new SparkConf().setAppName(APPLICATION_NAME)
     val sc = new SparkContext(conf)
     val cache = sc.textFile(FILE_PATH + FILE_NAME, 1).map(line => {
       val REGEX_PATTERN(ip, size) = line
       (ip, if (size != null) size.toLong else 0)
     })
-
-    val map: Array[String] = executeComputations(cache)
-    map.foreach(println)
-
-    FileUtil.fullyDelete(new File(PARTS_FILE_NAME_PREFIX))
-    FileUtil.fullyDelete(new File(RESULT_FILE_NAME))
-
-    sc.parallelize(map).saveAsTextFile(PARTS_FILE_NAME_PREFIX)
-    merge(PARTS_FILE_NAME_PREFIX, RESULT_FILE_NAME)
+    (sc, cache)
   }
 
-  def executeComputations(cache: RDD[(String, Long)]): Array[String] = {
+  def saveResultToFile(sc: SparkContext, map: Array[String]): Unit = {
+    val partFilesPath: String = FILE_PATH + PARTS_FILE_NAME_PREFIX
+    val resultFilePath: String = FILE_PATH + RESULT_FILE_NAME
+
+    FileUtil.fullyDelete(new File(partFilesPath))
+    FileUtil.fullyDelete(new File(resultFilePath))
+
+    sc.parallelize(map).saveAsTextFile(partFilesPath)
+    merge(partFilesPath, resultFilePath)
+  }
+
+  def countAverageByteNumberValues(cache: RDD[(String, Long)]): Array[String] = {
     val key: RDD[(String, (Float, Int))] = cache.aggregateByKey((0.0f, 0))(
       (acc, size) => (acc._1 + size, acc._2 + 1),
       (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
--- src/test/scala/ru/hokan/TestMain.scala
+++ src/test/scala/ru/hokan/TestMain.scala
@@ -6,17 +6,73 @@ import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
 
 class TestMain extends FlatSpec with GivenWhenThen with Matchers {
 
-  "Shakespeare most famous quote" should "be counted" in {
-    Given("quote")
+  "Average data from same ips" should "be computed" in {
+    Given("one ip")
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Some Application")
     val sc = SparkContext.getOrCreate(conf)
     val values = Array(("ip1", 10L), ("ip1", 20L))
     val rdd: RDD[(String, Long)] = sc.parallelize(values)
 
-    When("count words")
-    val wordCounts = Main.executeComputations(rdd)
+    When("count ip average data")
+    val result = Main.countAverageByteNumberValues(rdd)
 
-    Then("words counted")
-    wordCounts should equal (Array("ip1, 15.0, 30.0"))
+    Then("average data counted")
+    result should equal (Array("ip1, 15.0, 30.0"))
+  }
+
+  "Average data from separate different ips" should "be computed" in {
+    Given("multiple ip")
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Some Application")
+    val sc = SparkContext.getOrCreate(conf)
+    val values = Array(("ip1", 10L), ("ip2", 20L))
+    val rdd: RDD[(String, Long)] = sc.parallelize(values)
+
+    When("count ip average data")
+    val result = Main.countAverageByteNumberValues(rdd)
+
+    Then("average data counted")
+    result should equal (Array("ip2, 20.0, 20.0", "ip1, 10.0, 10.0"))
+  }
+
+  "Average data from connected different ips" should "be computed" in {
+    Given("multiple ip")
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Some Application")
+    val sc = SparkContext.getOrCreate(conf)
+    val values = Array(("ip1", 10L), ("ip2", 20L), ("ip1", 30L), ("ip2", 40L))
+    val rdd: RDD[(String, Long)] = sc.parallelize(values)
+
+    When("count ip average data")
+    val result = Main.countAverageByteNumberValues(rdd)
+
+    Then("average data counted")
+    result should equal (Array("ip2, 30.0, 60.0", "ip1, 20.0, 40.0"))
+  }
+
+  "Average data from separate different ips" should "NOT be equal to simple string" in {
+    Given("multiple ip")
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Some Application")
+    val sc = SparkContext.getOrCreate(conf)
+    val values = Array(("ip1", 10L), ("ip2", 20L))
+    val rdd: RDD[(String, Long)] = sc.parallelize(values)
+
+    When("count ip average data")
+    val result = Main.countAverageByteNumberValues(rdd)
+
+    Then("average data counted")
+    result should not equal Array("ip1")
+  }
+
+  "Average data from separate different ips" should "NOT be equal to string with wrong sorting order" in {
+    Given("multiple ip")
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Some Application")
+    val sc = SparkContext.getOrCreate(conf)
+    val values = Array(("ip1", 10L), ("ip2", 20L))
+    val rdd: RDD[(String, Long)] = sc.parallelize(values)
+
+    When("count ip average data")
+    val result = Main.countAverageByteNumberValues(rdd)
+
+    Then("average data counted")
+    result should not equal Array("ip1, 10.0, 10.0", "ip2, 20.0, 20.0")
   }
 }
