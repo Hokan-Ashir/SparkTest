281ce20d66cd954c60c3f25646809f166a18cc4f
Anton_Achkasov <achkasov.anton.92@gmail.com>
2016-09-12 22:04:06 +0300

    HW_3 completed, with not so awesome lined ROC

--- src/main/scala/ru/hokan/MLResultParser.scala
+++ src/main/scala/ru/hokan/MLResultParser.scala
@@ -8,6 +8,7 @@ object MLResultParser {
   val FLOAT_REGEXP = "\\d\\.\\d+"
   val REGEXP_PATTERN = "\\[(" + FLOAT_REGEXP + "),\\[(" + FLOAT_REGEXP + "),(" + FLOAT_REGEXP + ").*,(" + FLOAT_REGEXP + ")\\]"
   val REGEXP = REGEXP_PATTERN.r
+  val INTERVAL_RANGE_IN_PERCENT = 0.05
 
   def main(args: Array[String]): Unit = {
     val tuples = ListBuffer[Tuple]()
@@ -17,8 +18,40 @@ object MLResultParser {
       tuples += Tuple(expected.toFloat, probabilityHigh.toFloat, probabilityLow.toFloat, actual.toFloat)
     }
 
-    tuples.foreach(println)
+    val rates = ListBuffer[Rate]()
+    val BLOCK_SIZE: Double = tuples.length * INTERVAL_RANGE_IN_PERCENT
+    var j = 0
+    val tempValues: Values = Values(0, 0, 0, 0)
+    tuples.foreach(tuple => {
+
+      if (tuple.actual == 0 && tuple.expected == 0) {
+        tempValues.TN += 1
+      } else if (tuple.actual == 0 && tuple.expected == 1) {
+        tempValues.FN += 1
+      } else if (tuple.actual == 1 && tuple.expected == 0) {
+        tempValues.FP += 1
+      } else if (tuple.actual == 1 && tuple.expected == 1) {
+        tempValues.TP += 1
+      }
+
+      if (j > BLOCK_SIZE) {
+        val tpr: Float = tempValues.TP// / (tempValues.TP + tempValues.FN)
+        val fpr: Float = tempValues.FP// / (tempValues.FP + tempValues.TN)
+        rates += Rate(tpr, fpr)
+        j = 0
+      } else {
+        j += 1
+      }
+    })
+
+    val map: ListBuffer[(Float, Float)] = rates.map(rate => (rate.TPR / rates.last.TPR, rate.FPR / rates.last.FPR))
+    println("TPR")
+    map.foreach(element => println(element._1))
+    println("FPR")
+    map.foreach(element => println(element._2))
   }
 
   private case class Tuple(expected : Float, probabilityHigh : Float, probabilityLow : Float, actual : Float)
+  private case class Rate(TPR : Float, FPR : Float)
+  private case class Values(var TP : Float, var FP : Float, var TN : Float, var FN : Float)
 }
